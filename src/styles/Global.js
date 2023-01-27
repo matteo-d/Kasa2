04720c7049c2093b4ade59968784a0dd51c4d2b3
matteo_d <matteodrouin33@gmail.com>
2023-01-27 07:51:56 +0100

    save : Header responsive

--- src/styles/Global.js
+++ src/styles/Global.js
@@ -1,5 +1,5 @@
 import { createGlobalStyle, css } from 'styled-components'
- 
+
 const GlobalStyle = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap');
 
@@ -7,23 +7,17 @@ const GlobalStyle = createGlobalStyle`
     margin : 0;
     padding : 0;
     box-sizing: border-box;
+    font-family: 'Montserrat', sans-serif; 
     min-height : 100vh;
     margin : auto;
-    font-family: 'Montserrat', sans-serif; 
     --primary-color : #FF6060;
     --secondary-color : #FFFFFF;
     --tertiary-color : #F7F7F7;
     --weight-little : 400;
     --weight-medium : 500;
     --weight-big : 700;
-    --base-component-width : 86%;
+    --base-component-width : 86%; 
     }
-    body {
-    margin : 0;
-    padding : 0;
-    box-sizing: border-box;
-    min-height: 100vh;
-  }
   `
 
 export default GlobalStyle
--- src/styles/Header.js
+++ src/styles/Header.js
@@ -1,38 +1,111 @@
 import styled from "styled-components"
 import { Link } from "react-router-dom"
+import { device } from "../assets/device"
 
-const StyledHeader = styled.header`  
+
+const ContainerHeader = styled.header`  
   display : flex;
   justify-content : space-between;
   margin : auto;
   padding : 2rem 0 3rem 0;
   width : var(--base-component-width);
+  
 `
-const Img = styled.img`
-height : 2.8rem;
+const LogoKasa = styled.img`
+@media screen and (${device.prehistoricPhone}){
+height : 1.9rem;
+@media screen and (${device.mobileS}){
+height : 1.9rem;
+}
+@media screen and (${device.mobileM}){
+height : 2.5rem;
+}
+@media screen and (${device.mobileL}){
+height : 3rem;
+}
+@media screen and (${device.tablet}){
+height : 5rem;
+}
+@media screen and (${device.laptop}){
+height : 6rem;
+}
+@media screen and (${device.laptopL}){
+height : 7rem;
+}
+@media screen and (${device.desktop}){
+height : 8rem;
+}
+@media screen and (${device.desktopL}){
+height : 9rem;
+}
 `
-const Nav = styled.nav`
+const ContainerNav = styled.nav`
   align-self : center;
 `
-const Home = styled(Link)`
-  font-style: normal;
-  font-weight: var(--weight-medium);
-  font-size: 1.65rem;
+const LinkHome = styled(Link)`
+@media screen and (${device.prehistoricPhone}){
+font-size : 0.8rem;
+}@media screen and (${device.mobileS}){
+ 
+font-size : 1rem;
+}@media screen and (${device.mobileL}){
+  
+font-size : 1.2rem;
+}@media screen and (${device.tablet}){
+  
+font-size : 1.4rem;
+}@media screen and (${device.laptop}){
+  
+font-size : 1.6rem;
+}@media screen and (${device.laptopL}){
+  
+font-size : 1.8rem;
+}@media screen and (${device.desktop}){
+  
+font-size : 2rem;
+}@media screen and (${device.desktopL}){
+  
+font-size : 2.2rem;
+} 
+
   text-decoration-line: none;
   color: var(--primary-color);
+  font-weight: var(--weight-medium);
   &:active, &:hover {
     text-decoration-line : underline;
   }
 `
-const About = styled(Link)`
-  padding : 0rem 2rem 0rem 4rem;
-  font-style: normal;
-  font-weight: var(--weight-medium);
-  font-size: 1.65rem;
+const LinkAbout = styled(Link)`
+@media screen and (${device.prehistoricPhone}){
+  font-size : 0.8rem;
+  padding-left : 0.3rem;
+}@media screen and (${device.mobileS}){
+  padding-left : 0.3rem;
+  font-size : 1rem;
+}@media screen and (${device.mobileL}){
+  padding-left : 0.5rem;
+font-size : 1.2rem;
+}@media screen and (${device.tablet}){
+  padding-left : 0.5rem;
+font-size : 1.4rem;
+}@media screen and (${device.laptop}){
+  padding-left : 0.7rem;
+font-size : 1.6rem;
+}@media screen and (${device.laptopL}){
+  padding-left : 0.7rem;
+font-size : 1.8rem;
+}@media screen and (${device.desktop}){
+  padding-left : 0.9rem;
+font-size : 2rem;
+}@media screen and (${device.desktopL}){
+  padding-left : 0.9rem; 
+font-size : 2.2rem;
+}
+  font-weight: var(--weight-medium); 
   text-decoration-line: none;
   color: var(--primary-color); 
   &:active, &:hover {
     text-decoration-line : underline;
   } `
 
-export { StyledHeader, Img, Nav, Home, About }
+export { ContainerHeader, LogoKasa, ContainerNav, LinkHome, LinkAbout }
